e445d298bbf5f782e7fb2a05186b45dd50527934
Bogdan Kolomiets <kolom996@gmail.com>
2016-07-27 11:35:55 +0300

    #3 Added HTTP constants

--- app/src/main/java/com/example/bogdan/skyapplication/Constants.java
+++ app/src/main/java/com/example/bogdan/skyapplication/Constants.java
@@ -6,4 +6,10 @@ package com.example.bogdan.skyapplication;
  * @date 27.07.16
  */
 public class Constants {
+
+  public static class HTTP {
+    public static final String BASE_URL = "http://api.openweathermap.org/data/2.5/";
+    public static final String APPID = "c98b71c0d933a1a3c02ba2acc0342826";
+    public static final String UNITS = "metric";
+  }
 }
