acf571dbc66105a4e3e2a7148853e470328433ec
Bogdan Kolomiets <kolom996@gmail.com>
2016-07-27 12:44:56 +0300

    #6 Creating implementation Model and provided it with Dagger

--- app/src/main/java/com/example/bogdan/skyapplication/di/module/AppModule.java
+++ app/src/main/java/com/example/bogdan/skyapplication/di/module/AppModule.java
@@ -3,11 +3,17 @@ package com.example.bogdan.skyapplication.di.module;
 import android.content.Context;
 
 import com.example.bogdan.skyapplication.App;
+import com.example.bogdan.skyapplication.api.WeatherApi;
+import com.example.bogdan.skyapplication.model.Model;
+import com.example.bogdan.skyapplication.model.ModelImpl;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * @author Bogdan Kolomiets
@@ -26,5 +32,19 @@ public class AppModule {
   @Provides
   Context provideApplication() {
     return mApplication;
+  }
+
+  @Singleton
+  @Provides
+  Observable.Transformer provideSchedulerTransformer() {
+    return o -> ((Observable) o).subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .unsubscribeOn(Schedulers.io());
+  }
+
+  @Singleton
+  @Provides
+  Model provideModel(Observable.Transformer schedulerTransformer, WeatherApi api) {
+    return new ModelImpl(schedulerTransformer, api);
   }
 }
--- app/src/main/java/com/example/bogdan/skyapplication/model/ModelImpl.java
+++ app/src/main/java/com/example/bogdan/skyapplication/model/ModelImpl.java
@@ -1,7 +1,11 @@
 package com.example.bogdan.skyapplication.model;
 
+import com.example.bogdan.skyapplication.Constants;
+import com.example.bogdan.skyapplication.api.WeatherApi;
 import com.example.bogdan.skyapplication.model.entity.ForecastData;
 import com.example.bogdan.skyapplication.model.entity.WeatherData;
+
+import javax.inject.Inject;
 
 import rx.Observable;
 
@@ -11,14 +15,32 @@ import rx.Observable;
  * @date 27.07.16
  */
 public class ModelImpl implements Model {
+  private final Observable.Transformer mSchedulerTransformer;
+  private final WeatherApi mApiInterface;
+
+  @Inject
+  public ModelImpl(Observable.Transformer schedulerTransformer, WeatherApi apiInterface) {
+    mSchedulerTransformer = schedulerTransformer;
+    mApiInterface = apiInterface;
+  }
 
   @Override
   public Observable<WeatherData> getCurrentWeatherByCity(String city) {
-    return null;
+    return mApiInterface
+        .currentWeatherByCity(city, Constants.HTTP.UNITS, "ru", Constants.HTTP.APPID)
+        .compose(applySchedulers());
+
   }
 
   @Override
   public Observable<ForecastData> getWeekForecastByCity(String city) {
-    return null;
+    return mApiInterface
+        .weekForecastByCity(city, Constants.HTTP.UNITS, "ru", Constants.HTTP.APPID)
+        .compose(applySchedulers());
+  }
+
+  @SuppressWarnings("unchecked")
+  private <T> Observable.Transformer<T, T> applySchedulers() {
+    return (Observable.Transformer<T, T>) mSchedulerTransformer;
   }
 }
